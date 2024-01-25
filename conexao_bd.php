f9fff0b06cb6a820aa635ebef7083c85a3cfc291
luccaleca <122623089+luccaleca@users.noreply.github.com>
2024-01-25 17:51:29 -0300

    first commit

---
+++ conexao_bd.php
@@ -0,0 +1,10 @@
+<?php
+
+//Cabecalhos obrigatorios
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+//Incluir a conexao
+include_once '../conexao.php';
+
+//$id = 3;
